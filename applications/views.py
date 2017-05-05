d9829d40a94fd25b2e7c367baa71c93f6b5a7be1
Jimmy <zhengge2012@gmail.com>
2017-05-05 18:05:23 +0800

    add download images fuction

--- applications/views.py
+++ applications/views.py
@@ -89,3 +89,69 @@ class auth(basehttphander):
         if next_url:
             return redirect(next_url)
         return redirect(getsettings('url_prefix','/'))
+
+class DownloadHandler(basehttphander):
+    def get(self, path, include_body=True):
+        session_dir = self.settings['session_dir']
+        user = self.current_user
+        if user and 'session' in user:
+            session = user['session']
+        else:
+            logger.error(_("DownloadHandler: Could not determine use session"))
+            return # Something is wrong
+        filepath = os.path.join(session_dir, session, 'downloads', path)
+        abspath = os.path.abspath(filepath)
+        if not os.path.exists(abspath):
+            self.set_status(404)
+            self.write(self.get_error_html(404))
+            return
+        if not os.path.isfile(abspath):
+            raise tornado.web.HTTPError(403, "%s is not a file", path)
+        import stat, mimetypes
+        stat_result = os.stat(abspath)
+        modified = datetime.fromtimestamp(stat_result[stat.ST_MTIME])
+        self.set_header("Last-Modified", modified)
+        mime_type, encoding = mimetypes.guess_type(abspath)
+        if mime_type:
+            self.set_header("Content-Type", mime_type)
+        # Set the Cache-Control header to private since this file is not meant
+        # to be public.
+        self.set_header("Cache-Control", "private")
+        # Add some additional headers
+        self.set_header('Access-Control-Allow-Origin', '*')
+        # Check the If-Modified-Since, and don't send the result if the
+        # content has not been modified
+        ims_value = self.request.headers.get("If-Modified-Since")
+        if ims_value is not None:
+            import email.utils
+            date_tuple = email.utils.parsedate(ims_value)
+            if_since = datetime.fromtimestamp(time.mktime(date_tuple))
+            if if_since >= modified:
+                self.set_status(304)
+                return
+        # Finally, deliver the file
+        with io.open(abspath, "rb") as file:
+            data = file.read()
+            hasher = hashlib.sha1()
+            hasher.update(data)
+            self.set_header("Etag", '"%s"' % hasher.hexdigest())
+            if include_body:
+                self.write(data)
+            else:
+                assert self.request.method == "HEAD"
+                self.set_header("Content-Length", len(data))
+
+    def get_error_html(self, status_code, **kwargs):
+        self.require_setting("static_url")
+        if status_code in [404, 500, 503, 403]:
+            filename = os.path.join(self.settings['static_url'], '%d.html' % status_code)
+            if os.path.exists(filename):
+                with io.open(filename, 'r') as f:
+                    data = f.read()
+                return data
+        import httplib
+        return "<html><title>%(code)d: %(message)s</title>" \
+                "<body class='bodyErrorPage'>%(code)d: %(message)s</body></html>" % {
+            "code": status_code,
+            "message": httplib.responses[status_code],
+        }
